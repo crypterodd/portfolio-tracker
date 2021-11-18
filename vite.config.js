f93a0ae03421c0d516bc8166480ad4b0fe421422
Joan Picornell <joan.picornell@gmail.com>
2021-11-18 15:09:11 +0100

    Added base_url environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+const { resolve } = require('path')
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: process.env.BASE_URL || '/'
 })
